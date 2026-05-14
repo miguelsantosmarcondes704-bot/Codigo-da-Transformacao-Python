83b6b406ec313eff1432864f9e00c269d6c1e5ef
progamacao-cdtl <miguelsantosmarcondes704@gmail.com>
2026-05-14 16:55:34 -0300

    update

---
+++ modulo_02/modulo_02.py
@@ -0,0 +1,13 @@
+print("Olá Mundo!")
+
+numero = 10
+texto = "Python"
+decimal = 5.5
+
+print(type(numero))
+print(type(texto))
+print(type(decimal))
+
+nome = input("Digite seu nome: ")
+
+print("Olá,", nome, "! Seja bem-vindo ao Python.")
